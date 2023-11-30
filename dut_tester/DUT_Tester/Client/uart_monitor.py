5e2df6bffa29d2a3ddbf710c062d8ef068c01f2b
Endres Forlin, Bruno (UT-EEMCS) <b.endresforlin@utwente.nl>
2023-11-30 15:13:17 +0100

    added frame decoding

--- dut_tester/DUT_Tester/Client/uart_monitor.py
+++ dut_tester/DUT_Tester/Client/uart_monitor.py
@@ -8,6 +8,9 @@ import pigpio
 from DUT_Tester.log_id import CLIENT_SERIAL_TIMEOUT, CLIENT_SERIAL_RX
 from DUT_Tester.util import Logger
 
+from power_switch.error_codes import ErrorCodes
+import power_switch.powerswitch as ps
+
 SERIAL_TIMEOUT = 20
 
 
@@ -53,23 +56,22 @@ class UARTMonitor(Thread):
             data_avaiable = self.PI.serial_data_available(serial)
             if data_avaiable:
                 _, d = self.PI.serial_read(serial)
-                d = d.rstrip(b"\x00")
+                # d = d.rstrip(b"\x00")
                 if len(d) > 0:
                     self.serial_timeout = False
                     self.last_serial = time.time()
-                    try:
-                        string = d.decode("utf-8")
-                    except:
-                        string = str(binascii.hexlify(d), "ascii")
+
+                    self.check_for_frame(d)
+
                     self.logger.dataLogger.info(
                         {
                             "type": "Serial",
                             "id": CLIENT_SERIAL_RX,
                             "timestamp": time.time(),
-                            "data": string,
+                            "data": d,
                         }
                     )
-                    self.logger.consoleLogger.info("[Serial] " + string.rstrip("\n"))
+                    self.logger.consoleLogger.info("[Serial] " + d.rstrip("\n"))
 
             if (
                 time.time() - self.last_serial > SERIAL_TIMEOUT
@@ -86,7 +88,64 @@ class UARTMonitor(Thread):
                     }
                 )
 
+                ## ADD POWER CYCLE
+                # ps.
+
             time.sleep(1 / self.freq)
 
         self.PI.serial_close(serial)
         self.logger.consoleLogger.info(f"Stopped Data Monitor Thread. [{os.getpid()}]")
+
+    def check_for_frame(self, buffer):
+        # Read byte by byte
+
+        frame_buffer = bytearray()
+        in_frame = False
+        payload_length = 0
+        bytes_read = 0
+
+        for byte in buffer:
+            if byte == b"\xAA":  # Start of frame
+                in_frame = True
+                frame_buffer = bytearray(byte)
+                bytes_read = 0
+            elif in_frame:
+                frame_buffer.append(byte[0])
+                bytes_read += 1
+                if bytes_read == 2:  # Assuming the second byte is payload_length
+                    payload_length = byte[0]
+
+                if (
+                    bytes_read > 2 and bytes_read == payload_length + 5
+                ):  # +3 for start_of_frame, frame_id, and payload_length itself
+                    if byte == b"\x55":  # End of frame
+                        self.decode_frame(frame_buffer)
+                        in_frame = False
+                    else:
+                        print("Frame error: Incorrect end of frame byte")
+                        in_frame = False
+
+    def decode_frame(self, frame_bytes):
+        # TODO: Implement frame decoding based on your protocol
+        print(f"Received frame: {frame_bytes}")
+
+        header = frame_bytes[0]
+        frame_id = frame_bytes[1]
+        payload_length = frame_bytes[2]
+        payload = frame_bytes[3 : 3 + payload_length]
+        crc_bytes = frame_bytes[3 + payload_length : 5 + payload_length]
+        tail = frame_bytes[-1]
+
+        # Concatenating the CRC bytes into a single number
+        # Assuming CRC is in big-endian format
+        crc = (crc_bytes[0] << 8) | crc_bytes[1]
+
+        # Converting payload to hex representation
+        payload_hex = [hex(byte) for byte in payload]
+
+        print(f"Header: {hex(header)}")
+        print(f"Payload Length: {hex(payload_length)}")
+        print(f"Frame ID: {hex(frame_id)}")
+        print(f"Payload: {payload_hex}")
+        print(f"CRC: {hex(crc)}")
+        print(f"Tail: {hex(tail)}")
